3698395ee6ae987919647ed9f34a0516a654383c
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-11-04 11:34:18 +0200

    added header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  loop: true,
+
+  navigation: {
+    nextEl: '.slider__navigation--next',
+    prevEl: '.slider__navigation--prev',
+  },
+});
+
+document.documentElement.style.setProperty(
+  '--scrollbar-width',
+  `${window.innerWidth - document.documentElement.clientWidth}px`
+);
